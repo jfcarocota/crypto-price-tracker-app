c9ab1bdb803df06f7e23cb1f17250f9a6dd3e334
jcaro <jfcarocota@gmail.com>
2024-07-03 16:48:26 -0700

    just type yarn on terminal and will install al packages.

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import currenciesReducer from './slices/currenciesSlice';
 
 const store = configureStore({
